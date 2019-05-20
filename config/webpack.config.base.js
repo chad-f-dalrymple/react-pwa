9317755ee1448b084e1e04824ec8362ee41a5b6c
Chad <cdalrymple@connectivedx.com>
2019-05-19 20:10:44 -0400

    first commit

---
+++ config/webpack.config.base.js
@@ -0,0 +1,102 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ChunkRenamePlugin = require("chunk-rename-webpack-plugin");
+const CorberWebpackPlugin = require('corber-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  context: path.resolve(__dirname, '../src'),
+  entry: {
+    app: ['./index.js'],
+    secondary: ['./secondary.js']
+  },
+  output: {
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
+    path: path.resolve(__dirname, '../dist')
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    port: 8080,
+    stats: {
+      children: false
+    }
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[name].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader'
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [{
+          loader: 'html-loader',
+          options: {
+            minimize: true
+          }
+        }]
+      }
+    ]
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new ChunkRenamePlugin({
+      app: 'app.js',
+      secondary: 'secondary.js'
+    }),
+    new CorberWebpackPlugin(),
+    new CleanWebpackPlugin(['dist']),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css',
+      chunkFilename: '[id].[hash].css'
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../src/html/index.html'),
+      filename: 'index.html'
+    }),
+  ]
+};
